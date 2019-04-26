e9cca66c495fb1650337896cdefd552313d36e3f
ZYHowell <1148830469@qq.com>
2019-04-26 14:18:52 +0800

    really_no_bug

--- back_end/test.cpp
+++ back_end/test.cpp
@@ -1,26 +1,44 @@
 #include "bplustree.hpp"
 #include <cstring>
-int it = 2333;
-int fake_random(int a){
-    return (a * 23) % 10007;
+#include <algorithm>
+int it = 23333;
+const unsigned Ta = 33333331, Tb = 1 << 16 | 3;
+unsigned Tc;
+inline unsigned randint()
+{
+    return Tc = (Tc * Ta + Tb);
 }
-bool judgement(int a, int b){
-    return a < b && 2 * a < b;
+int fake_random(int a){
+    return (a * 23) % 100007;
+}
+struct test_str{
+    int a,b;
+    test_str(int aa = 0, int bb = 0):a(aa),b(bb){}
+};
+bool judgement(test_str a, test_str b){
+    return a.a < b.a;
+}
+bool operator<(test_str a, test_str b){
+    if (a.a != b.a) return a.a < b.a;
+    return a.b < b.b;
 }
 int main(){
-    bplustree<int, int, 4> test;
+    bplustree <test_str, int, 4> test;
     test.init("data.txt", "alloc.txt");
-    // test.clear();
-    int its[100];
+    test.clear();
+    int its[10000];int con = 0;
     for (int i = 0;i < 100;i++){
-        its[i] = it = fake_random(it);
-        // test.insert(its[i], its[i] * 2);
+        its[i] = randint() % 50;
+        test.insert(test_str(its[i], con), its[i] * 2);
+        printf("%d %d; ", its[i], con++);
     }
-    for (int i = 0;i < 100;i++){
-        printf("should be: %d ", its[i] * 2);
-        printf("it is: %d\n", test.find(its[i]));
-    }
-    vector<pair<int, int>> v = test.listof(7352 / 2, judgement);
-    for (int i = 0;i < v.size();i++) printf("%d %d; ", v[i].first, v[i].second);
+    // for (int i = 0;i < 10000;i++){
+    //     printf("should be: %d ", its[i] * 2);
+    //     printf("it is: %d\n", test.find(its[i]));
+    // }
+    std::sort(its, its + 100);
+    for (int i = 0;i < 100;i++) printf("%d ", its[i]);
+    vector<pair<test_str, int>> v = test.listof(test_str(15, 0), judgement);
+    for (int i = 0;i < v.size();i++) printf("%d %d %d; ", v[i].first.a, v[i].first.b, v[i].second);
     return 0;
 }
